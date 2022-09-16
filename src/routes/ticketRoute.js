072b0089b9eb8ee7740530159535dcab39b11b4d
Mauro Domingues <104386910+Mauro-Domingues@users.noreply.github.com>
2022-09-16 17:53:44 -0300

    Delete src directory

--- src/routes/ticketRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const ticketController = require('../controllers/ticketController')
-
-router.post('/', ticketController.post)
-
-module.exports = router
